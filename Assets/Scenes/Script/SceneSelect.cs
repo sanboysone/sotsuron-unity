c3717daecac7b85c37782f1579a65b38555c6ab5
Takanori Sudo <sanboysoney3@gmail.com>
2018-08-04 00:38:40 +0900

    シーンの移動完成

--- Assets/Scenes/Script/SceneSelect.cs
+++ Assets/Scenes/Script/SceneSelect.cs
@@ -1,12 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneSelect : MonoBehaviour {
 
+	public string[] textMessage; //テキストの加工前の一行を入れる変数
+	public static string UrlString;
+	
 	// Use this for initialization
 	void Start () {
+		TextAsset textasset = new TextAsset(); //テキストファイルのデータを取得するインスタンスを作成
+		textasset = Resources.Load("URL", typeof(TextAsset) )as TextAsset; //Resourcesフォルダから対象テキストを取得
+		string TextLines = textasset.text; //テキスト全体をstring型で入れる変数を用意して入れる
 		
+		Debug.Log(TextLines);
+
+		//Splitで一行づつを代入した1次配列を作成
+		textMessage = TextLines.Split('¥'); 
+		
+		Debug.Log(textMessage[0]);
 	}
 	
 	// Update is called once per frame
@@ -14,8 +28,15 @@ public class SceneSelect : MonoBehaviour {
 		
 	}
 
-	public void OnClick()
-	{
-		
+	public void OnClick(){
+		if (textMessage == null || textMessage[0].Equals("none"))
+		{
+			SceneManager.LoadScene ("school_select"); //学校のurlを入力するシーン
+		}
+		else
+		{
+			UrlString = textMessage[0];
+			SceneManager.LoadScene ("main"); //学校のurlが入力済みだった場合はメイン画面に行く
+		}
 	}
 }
